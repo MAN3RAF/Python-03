dcc21a9096e03b01036c8dd79b8bc91f3bfa289e
MyGit011 <mypc20339@gmail.com>
2026-01-10 10:52:19 +0100

    up

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+import sys
+
+program_name = sys.argv[0]
+program_len = len(sys.argv)
+
+print("=== Command Quest ===")
+
+if program_len < 2:
+    print("No arguments provided!")
+    print(f"program name: {program_name}")
+    print(f"Total arguments: {program_len}")
+else:
+    print(f"Program name: {program_name}")
+    print(f"Arguments received: {program_len - 1}")
+    for i in range(1, len(sys.argv)):
+        print(f"Argument {i}: {sys.argv[i]}")
+    print(f"Total arguments: {program_len}")
---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,13 @@
+import sys
+
+program_len = len(sys.argv)
+
+if program_len < 2:
+    print(
+    "No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ..."
+    )
+
+else:
+    for i in range(1, program_len):
+        pass
+    score_list = []
