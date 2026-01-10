1b4aac1239fe8f37035485c6d4f0553e93f797d9
Lahcen Sebar <lsebar@c3r2p5.1337.ma>
2026-01-10 14:50:30 +0100

    up

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -1,13 +1,23 @@
 import sys
 
 program_len = len(sys.argv)
-
+print("=== Player Score Analytics ===")
 if program_len < 2:
     print(
     "No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ..."
     )
 
 else:
-    for i in range(1, program_len):
-        pass
     score_list = []
+    try:
+        for i in range(1, program_len):
+            score_list.append(int(sys.argv[i]))
+        print(f"Scores processed: {score_list}")
+        print(f"Total players: {program_len - 1}")
+        print(f"Total score: {sum(score_list)}")
+        print(f"Average score: {sum(score_list) / (program_len - 1)}")
+        print(f"High score: {max(score_list)}")
+        print(f"Low score: {min(score_list)}")
+        print(f"Score range: {max(score_list) - min(score_list)}")
+    except ValueError:
+        print("Error: All the scores must be numbers!")
--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -5,29 +5,54 @@ position = tuple([10, 20, 5])
 spawn = tuple([0, 0, 0])
 #coords = position.split(",")
 
-x1 = position[0]
-y1 = position[1]
-z1 = position[2]
+x1, y1, z1 = position
 
-x2 = 0
-y2 = 0
-z2 = 0
+x2, y2, z2 = spawn
 
 distance = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
 
 print("=== Game Coordinate System ===\n")
 
 print(f"Position created: {position}")
-print(f"Distance between {spawn} and {position}: {distance: .2f}\n")
+print(f"Distance between {spawn} and {position}: {distance:.2f}")
 
-position = tuple("1,sfgaea,fgs".split(","))
+print("")
+
+coords = "3,4,0"
+print(f'Parsing coordinates: "{coords}"')
 try:
-	x1 = int(position[0])
-	y1 = int(position[1])
-	z1 = int(position[2])
+	position = tuple(int(n) for n in coords.split(","))
+	x1, y1, z1 = position
 
-	print(x1, y1, z1)
+	distance = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+
+	print(f"Parsed position: ({x1}, {y1}, {z1})")
+	print(f"Distance between ({x2}, {y2}, {z2}) and ({x1}, {y1}, {z1}): {distance}")
 
 except ValueError as e:
 	print("Error parsing coordinates:", e)
 	print(f'Error details - Type: ValueError, Args: ("{e}",)')
+
+print("")
+
+coords = "abc,def,ghi"
+print(f'Parsing coordinates: "{coords}"')
+try:
+	position = tuple(int(n) for n in coords.split(","))
+
+	x1, y1, z1 = position
+
+	distance = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+
+	print(f"Parsed position: ({position})")
+	print(f"Distance between ({x2}, {y2}, {z2}) and ({position}): {distance}")
+
+except ValueError as e:
+	print("Error parsing coordinates:", e)
+	print(f'Error details - Type: ValueError, Args: ("{e}",)')
+
+print("")
+
+print("Unpacking demonstration:")
+print(f"Player at x={x1}, y={y1}, z={z1}")
+print(f"Coordinates: X={x1}, Y={y1}, Z={z1}")
