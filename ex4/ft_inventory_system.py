bb4bc15213e90ba63b16db62ab8763b11629d17d
Lahcen Sebar <lsebar@c3r3p3.1337.ma>
2026-01-11 16:41:36 +0100

    up

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,31 +1,59 @@
 
 
 alice_inventory = {
-	"sword":{
-		"type": "weapon",
-		"rarity":"rare",
-		"quantity": 1,
-		"value": 500
-		},
-	"potion":{
-		"type": "consumable",
-		"rarity": "common",
-		"quantity": 5,
-		"value": 50
-		},
-	"shield":{
-		"type": "armor",
-		"rarity": "uncommon",
-		"quntity": 1,
-		"value": 200
-		}
-	}
+    "sword":{
+        "type": "tgrt",
+        "rarity":"rare",
+        "quantity": 1,
+        "value": 500
+        },
+    "potion":{
+        "type": "consumable",
+        "rarity": "common",
+        "quantity": 5,
+        "value": 50
+        },
+    "shield":{
+        "type": "armor",
+        "rarity": "uncommon",
+        "quantity": 1,
+        "value": 200
+        }
+    }
 bob_inventory = {}
 
-sum_gold = alice_inventory['sword'].get("quantity") * alice_inventory["sword"].get("value")
-item_type = alice_inventory['sword'].get("type")
-item_rarity = alice_inventory['sword'].get("rarity")
-item_quantity = alice_inventory['sword'].get("quantity")
-item_value = alice_inventory['sword'].get("value")
+def get_inventory_info(inventory: dict):
+    inventory_value = item_count = 0
+    categories = {}
+    for k, v in inventory.items():
+        sum_gold = v.get("quantity") * v.get("value")
+        item_type = v.get("type")
+        item_rarity = v.get("rarity")
+        item_quantity = v.get("quantity")
+        item_value = v.get("value")
+        inventory_value += sum_gold
+        item_count += item_quantity
+        categories.update({item_type: item_quantity}) 
+        print(
+            f"{k} ({item_type}, {item_rarity}): "
+            f"{item_quantity}x @ {item_value} gold each = {sum_gold} gold"
+            )
+    print()
 
-print(f"sword ({item_type}, {item_rarity}): {item_quantity}x @ {item_value} gold each = {sum_gold} gold")
+    print(f"Inventory value: {inventory_value} gold")
+    
+    print(f"Item count: {item_count} items")
+    
+    print("Categories:", end=' ')
+    formatted = []
+    for k, v in categories.items():
+        formatted.append(f"{k}({v})")
+    print(", ".join(formatted))
+
+def transaction(alice_inv, bob_inv, item, many):
+    if alice_inv.get(item):
+        alice_inv[item]["quantity"] -= many
+        print("!!!!!")
+item = "sword"
+get_inventory_info(alice_inventory)
+transaction(alice_inventory, bob_inventory, item, 2)
