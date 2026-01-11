d8da15b6a960eff51ab53e1f16fc39c8e4993a0f
Lahcen Sebar <lsebar@c3r3p3.1337.ma>
2026-01-11 19:03:41 +0100

    up

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,8 +1,7 @@
 
-
 alice_inventory = {
     "sword":{
-        "type": "tgrt",
+        "type": "weapon",
         "rarity":"rare",
         "quantity": 1,
         "value": 500
@@ -20,11 +19,21 @@ alice_inventory = {
         "value": 200
         }
     }
-bob_inventory = {}
 
-def get_inventory_info(inventory: dict):
+bob_inventory = {
+    "magic_ring":{
+        "type": "accessory",
+        "rarity": "rare",
+        "quantity": 1,
+        "value": 300
+        }
+}
+
+def get_inventory_info(inventory: dict) -> None:
+    """Display detailed inventory information including items, values, and categories."""
     inventory_value = item_count = 0
     categories = {}
+    print("\n=== Alice's Inventory ===")
     for k, v in inventory.items():
         sum_gold = v.get("quantity") * v.get("value")
         item_type = v.get("type")
@@ -49,11 +58,57 @@ def get_inventory_info(inventory: dict):
     for k, v in categories.items():
         formatted.append(f"{k}({v})")
     print(", ".join(formatted))
+	
+    print()
 
-def transaction(alice_inv, bob_inv, item, many):
+def transaction(alice_inv: dict, bob_inv: dict, item: str, many: int) -> None:
+    """Transfer items from Alice's inventory to Bob's inventory."""
     if alice_inv.get(item):
-        alice_inv[item]["quantity"] -= many
-        print("!!!!!")
-item = "sword"
+        item_quantity = alice_inv[item].get("quantity")
+        item_type = alice_inv[item].get("type")
+        item_rarity = alice_inv[item].get("rarity")
+        item_value = alice_inv[item].get("value")
+        if many == 0:
+           print("Error: Cannot give 0 items!")
+        elif item_quantity >= many:
+            alice_inv[item]["quantity"] -= many
+            if bob_inv.get(item):
+                bob_inv[item]["quantity"] += many
+            else:
+                bob_inv.update({item: {k: v} for k, v in alice_inv[item].items()})
+                bob_inv[item]["quantity"] = many
+            if alice_inv[item].get("quantity") == 0:
+                alice_inv.pop(item)
+            print(f"=== Transaction: Alice gives Bob {many} {item} ===\nTransaction successful!")
+        else:
+            print(f"Error: '{item}' quantity not enough!")
+    else:
+        print("Error: alice doesn't have that item!")
+    print()
+    print("=== Updated Inventories ===")
+    
+    if alice_inv.get(item):
+        alice_qty = alice_inv[item].get("quantity")
+    else:
+        alice_qty = 0
+    
+    if bob_inv.get(item):
+        bob_qty = bob_inv[item].get("quantity")
+    else:
+        bob_qty = 0
+
+    
+    print(f"Alice {item}: {alice_qty}")
+	
+    print(f"Bob {item}: {bob_qty}")
+
+
+print("=== Player Inventory System ===")
+
+item = "potion"
+
 get_inventory_info(alice_inventory)
+
 transaction(alice_inventory, bob_inventory, item, 2)
+
+print()
